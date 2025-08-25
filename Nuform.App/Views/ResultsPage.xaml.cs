5a77ca3bb1fef3537899c53e8a1cd2c4c554b547
MrDman1 <damian.beland@hotmail.com>
2025-08-25 11:48:51 -0400

    test

--- Nuform.App/Views/ResultsPage.xaml.cs
+++ Nuform.App/Views/ResultsPage.xaml.cs
@@ -1,12 +1,14 @@
 using System.Windows.Controls;
-using Nuform.Core.Domain;
 using Nuform.App.ViewModels;
+
+// Alias the Core type so there’s no ambiguity
+using CoreEstimateState = Nuform.Core.Domain.EstimateState;
 
 namespace Nuform.App.Views
 {
     public partial class ResultsPage : Page
     {
-        public ResultsPage(EstimateState state)
+        public ResultsPage(CoreEstimateState state)
         {
             InitializeComponent();
             DataContext = new ResultsViewModel(state);
